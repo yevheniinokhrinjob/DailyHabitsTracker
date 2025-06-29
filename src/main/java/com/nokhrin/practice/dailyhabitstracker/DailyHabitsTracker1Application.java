6fd9942a10593f9ce3bd2cdc6115976e16ba30c5
yevheniinokhrin <233315@edu.p.lodz.pl>
2025-06-29 16:40:52 +0200

    init commit

---
+++ src/main/java/com/nokhrin/practice/dailyhabitstracker/DailyHabitsTracker1Application.java
@@ -0,0 +1,13 @@
+package com.nokhrin.practice.dailyhabitstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyHabitsTracker1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyHabitsTracker1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nokhrin/practice/dailyhabitstracker/DailyHabitsTracker1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nokhrin.practice.dailyhabitstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyHabitsTracker1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
